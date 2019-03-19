afba56f6e8d25e8ac06e106b556f6159e987a8be
Allex Oliveira <allex.oliveira@yahoo.com.br>
2019-03-19 17:39:14 +0100

    fix upload task

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-git clone --depth=1 --branch=master https://github.com/oliveiraallex/pharothings-ci.git
-cp download/server.zip pharothings-ci/docs/
-cp download/multi.zip pharothings-ci/docs/
-cp download/client.zip pharothings-ci/docs/
-cd pharothings-ci
+git clone --depth=1 --branch=master https://github.com/pharo-iot/Ci.git
+cp download/server.zip Ci/docs/
+cp download/multi.zip Ci/docs/
+cp download/client.zip Ci/docs/
+cd Ci
 git checkout master
 git add docs/*.zip
-git commit -m "Travis upload"
-git push http://oliveiraallex:${GH}@github.com/oliveiraallex/pharothings-ci.git > /dev/null 2>&1
+git commit -m "Travis upload release files"
+git push http://oliveiraallex:${GH}@github.com/pharo-iot/Ci.git > /dev/null 2>&1
