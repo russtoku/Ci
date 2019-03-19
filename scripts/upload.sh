b1ba9b2f7cc556a5d99c46d5c116505984d255a1
Allex Oliveira <allex.oliveira@yahoo.com.br>
2019-03-19 20:18:00 +0100

    fix upload task

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,7 +7,9 @@ cp download/server.zip Ci/docs/
 cp download/multi.zip Ci/docs/
 cp download/client.zip Ci/docs/
 cd Ci
+git config --global user.email "allex.oliveira@yahoo.com.br"
+git config --global user.name "Allex Oliveira"
 git checkout master
 git add docs/*.zip
 git commit -m "Travis upload release files"
-git push http://${GH}@github.com/pharo-iot/Ci.git 
+git push "http://${GH}@github.com/pharo-iot/Ci.git" master
